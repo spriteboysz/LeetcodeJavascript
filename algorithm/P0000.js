56888c9ad5896c6dfbcd80cd1567a391b5940e09
spriteboysz <sprite_bx@126.com>
2022-09-12 12:09:14 +0800

    2022091200

---
+++ algorithm/P0000.js
@@ -0,0 +1,12 @@
+/*
+        * Author: Deean
+        * Date: 2022-08-07 20:48
+        * FileName: P0000.测试网页
+        *
+        */
+function square(x) {
+    return x * x;
+}
+
+//alert(square(2));
+console.log(square(2));
